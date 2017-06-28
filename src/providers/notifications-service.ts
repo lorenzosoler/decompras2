1277a7a183ad896071fba8dc46866a6243fabf98
lorenzosoler <lorenzosoler5@gmail.com>
2017-06-28 19:33:19 -0300

    se agrega el icono de la app

--- src/providers/notifications-service.ts
+++ src/providers/notifications-service.ts
@@ -1,16 +1,23 @@
 import { Http, RequestOptions, Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
+import { User } from "../models/user";
+import { UserService } from "./user-service";
 
 @Injectable()
 export class NotificationsService {
+  private currentUser: User;
 
-  constructor(private http: Http) { }
+  constructor(private http: Http, private userService: UserService) {
+    this.currentUser = this.userService.getCurrentUser();
+  }
 
   public addUserList(list: any, user: any) {
     let body = {
         app_id: "8e4f03e5-8ffb-4fb8-9dd8-7136c5156202",
         included_segments: ["All"],
-        contents: {"en": "English Message"}
+        headings: {"en": this.currentUser.fullname },
+        contents: {"en": `Te agrego a la lista ${list.name}`},
+        large_icon: this.currentUser.photo
     };
     let headers = new Headers({ 'Authorization': 'Basic NzgyYmQ1NjktNTMwNS00MWRmLWExMTYtNjEyYWU2OTA5OGUy' });
     let options = new RequestOptions({ headers: headers })
