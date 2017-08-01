6ad68473dd8f0af53205752b04581c4a53b8f25a
lorenzosoler <lorenzosoler5@gmail.com>
2017-08-01 20:45:26 -0300

    se permite el login con google vinculado a la misma cuenta de facebook, si los email coinciden

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -33,32 +33,72 @@ export class LoginPage {
     if (this.platform.is('cordova')) {
       this.facebook.login(['public_profile', 'user_friends', 'email']).then( (response) => {
         const facebookCredential = firebase.auth.FacebookAuthProvider.credential(response.authResponse.accessToken);
-        firebase.auth().signInWithCredential(facebookCredential)
+        var auth = firebase.auth();
+        auth.signInWithCredential(facebookCredential)
         .then((success) => {
           this.userService.saveUser(new User(success)).then(()=> {
             this.loader.dismiss();
             this.navCtrl.setRoot(MisListasPage);
           });
         })
-        .catch((error) => {
+        .catch((error: any) => {
+          // An error happened.
+          if (error.code === 'auth/account-exists-with-different-credential') {
+            var pendingCred = error.credential;
+            // The provider account's email address.
+            var email = error.email;
+            // Get registered providers for this email.
+            auth.fetchProvidersForEmail(email).then( (providers) => {
+              this.googlePlus.login({
+                'webClientId': '955671816280-untuia24tvidjifk40qj1li0foip7rng.apps.googleusercontent.com',
+                'offline': true
+              }).then( res => {
+                firebase.auth().signInWithCredential(firebase.auth.GoogleAuthProvider.credential(res.idToken)).then((user) => {
+                  user.link(pendingCred).then(() => {
+                    // Facebook account successfully linked to the existing Firebase user.
+                    this.userService.saveUser(new User(user)).then(()=> {
+                      this.loader.dismiss();
+                      this.navCtrl.setRoot(MisListasPage);
+                    });
+                  });
+                });
+              });
+            });
+          }
           this.loader.dismiss();
           console.log("Firebase failure: " + JSON.stringify(error));
         });
 
       }).catch((error) => { this.loader.dismiss(); console.log(error); });
     } else {
-      this.af.auth.login({
-        provider: AuthProviders.Facebook,
-        method: AuthMethods.Popup
-      }).then((user) => {
-        this.userService.saveUser(new User(user.auth)).then(()=> {
+      var auth = firebase.auth();
+      auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())
+      .then((user) => {
+        this.userService.saveUser(new User(user.user)).then(()=> {
           this.loader.dismiss();
           this.navCtrl.setRoot(MisListasPage);
         });
-      }).catch(error=>{
-        this.loader.dismiss();
-        console.log(error);
       })
+      .catch( (error: any) => {
+        // An error happened.
+        if (error.code === 'auth/account-exists-with-different-credential') {
+          var pendingCred = error.credential;
+          // The provider account's email address.
+          var email = error.email;
+          // Get registered providers for this email.
+          auth.fetchProvidersForEmail(email).then( (providers) => {
+            auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then((result) => {
+              result.user.link(pendingCred).then(() => {
+                // Facebook account successfully linked to the existing Firebase user.
+                this.userService.saveUser(new User(result.user)).then(()=> {
+                  this.loader.dismiss();
+                  this.navCtrl.setRoot(MisListasPage);
+                });
+              });
+            });
+          });
+        }
+      });
     }
   }
 
