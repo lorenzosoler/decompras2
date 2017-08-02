9578f1c4b36278ab00a80471934b8810c15f933c
lorenzosoler <lorenzosoler5@gmail.com>
2017-08-01 22:46:38 -0300

    mensaje de alerta cuando se intenta acceder con facebook y ya existe una cuenta asociada a ese email

--- src/providers/auth-service.ts
+++ src/providers/auth-service.ts
@@ -3,16 +3,18 @@ import { AuthProviders, FirebaseAuthState, AuthMethods, AngularFire } from 'angu
 
 import { Platform } from 'ionic-angular';
 import { Facebook } from "@ionic-native/facebook";
-import { UserInfo } from "firebase";
 
 import firebase from 'firebase';
-import { User } from "../models/user";
 import { GooglePlus } from "@ionic-native/google-plus";
 
 @Injectable()
 export class AuthService {
 
-  constructor(public platform: Platform, public facebook:Facebook, public af: AngularFire, private google: GooglePlus ) { }
+  constructor(public platform: Platform, 
+  public facebook:Facebook,
+  public af: AngularFire, 
+  private google: GooglePlus) {
+   }
 
   logout() {
     return this.af.auth.logout().then(() => {
