1cfc843be99ab2c5cfc2a670a31a31129be6de80
lorenzo.soler <lorenzo@ntbflux-Lsoler>
2017-10-06 17:07:30 -0300

    fixes login y usuarios admin

--- src/pages/users-list/users-list.ts
+++ src/pages/users-list/users-list.ts
@@ -50,6 +50,7 @@ export class UsersListPage {
 
   private setUserAdmin (userId: string, msg: string) {
     this.listService.setUserAdmin(this.currentList.$key, userId).then((data) => {
+      this.currentList.admins[userId] = true;
       this.toastCtrl.create({
           message: msg,
           duration: 3000
@@ -59,6 +60,7 @@ export class UsersListPage {
 
   private deleteUserAdmin (userId: string, msg: string) {
     this.listService.deleteUserAdmin(this.currentList.$key, userId).then((data) => {
+      delete this.currentList.admins[userId];
       this.toastCtrl.create({
           message: msg,
           duration: 3000
@@ -73,11 +75,11 @@ export class UsersListPage {
         title: '',
         buttons: [
           {
-            text: this.isAdmin() ? 'Deshacer como admin' : data.HACERADMIN,
+            text: this.isUserAdmin(user.uid) ? 'Deshacer como admin' : data.HACERADMIN,
             icon: 'contact',
             cssClass: 'admin-action-sheet',
             handler: () => {
-              if (!this.isAdmin()) {
+              if (!this.isUserAdmin(user.uid)) {
                 this.setUserAdmin(user.$key, data.USUARIOADMIN);
               } else {
                 this.deleteUserAdmin(user.$key, "El usuario dejo de ser Admin de esta lista")
